eb67f784569edebaae1af5b5dccae2dcba07fbbb
PJ Frias <peejfrias@gmail.com>
2018-01-04 03:19:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,7 @@ def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
     if(ingredients.include?(cheese))
-      puts cheese
+      cheese
     end
   end
 end
